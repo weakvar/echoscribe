d5af056100a225e61f72fe4201b89908760e2904
Vladislav Len <vlad@weakvar.com>
2021-12-04 23:01:10 +0300

    ES-2 Prepared a project

---
+++ Echoscribe/Supporting Files/AppDelegate.swift
@@ -0,0 +1,42 @@
+//
+//  AppDelegate.swift
+//  Echoscribe
+//
+//  Created by Vladislav Len on 04.12.2021.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder {
+    
+    // MARK: - Public Properties
+    
+    var window: UIWindow?
+    
+    // MARK: - Private Methods
+    
+    private func setRootViewController() {
+        let viewController = UIViewController()
+        viewController.view.backgroundColor = .systemBackground
+        
+        let window = UIWindow(frame: UIScreen.main.bounds)
+        window.rootViewController = viewController
+        window.makeKeyAndVisible()
+        
+        self.window = window
+    }
+    
+}
+
+// MARK: - UIApplicationDelegate
+
+extension AppDelegate: UIApplicationDelegate {
+    
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        setRootViewController()
+        
+        return true
+    }
+    
+}
