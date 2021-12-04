1db0792eb672b4d4fb9b9da1108277358e2d6cc0
Vladislav Len <vlad@weakvar.com>
2021-12-04 22:38:40 +0300

    ES-1 Created a project

---
+++ Echoscribe/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Echoscribe
+//
+//  Created by Vladislav Len on 04.12.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
