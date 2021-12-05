1864b8f1670a67e753da75c5c8aad27be11691aa
Vladislav Len <vlad@weakvar.com>
2021-12-05 23:55:56 +0300

    ES-6 Created a data models for subscriptions

---
+++ Echoscribe/Data Models/Subscription.swift
@@ -0,0 +1,13 @@
+//
+//  Subscription.swift
+//  Echoscribe
+//
+//  Created by Vladislav Len on 05.12.2021.
+//
+
+import Foundation
+
+struct Subscription {
+    let service: SubscriptionService
+    let paymentDate: Date
+}
---
+++ Echoscribe/Data Models/SubscriptionService.swift
@@ -0,0 +1,13 @@
+//
+//  SubscriptionService.swift
+//  Echoscribe
+//
+//  Created by Vladislav Len on 05.12.2021.
+//
+
+import Foundation
+
+struct SubscriptionService: Codable {
+    let name: String
+    let logoUrl: String
+}
