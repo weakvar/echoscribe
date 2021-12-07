a0773aec042036a4e666eed7490d6de9c7c0e9da
Vladislav Len <vlad@weakvar.com>
2021-12-08 00:34:55 +0300

    ES-6 Created a client for networking

---
+++ Echoscribe/Services/Network/HTTPMethod.swift
@@ -0,0 +1,16 @@
+//
+//  HTTPMethod.swift
+//  Echoscribe
+//
+//  Created by Vladislav Len on 07.12.2021.
+//
+
+import Foundation
+
+enum HTTPMethod: String {
+    case get    = "GET"
+    case post   = "POST"
+    case put    = "PUT"
+    case delete = "DELETE"
+    case patch  = "PATCH"
+}
